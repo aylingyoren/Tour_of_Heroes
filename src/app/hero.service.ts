712a931e75f33992c956ae852bb262468f484113
Aylin Gyoren <aylingyoren21@gmail.com>
2021-07-04 14:15:13 +0300

    added save() n updateHero obs with http.put() n httpOptions to update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -42,4 +42,15 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 }
