8e46ac92ca728bef4c28e20a137f68883fb26dc5
aylingyoren <aylingyoren21@gmail.com>
2021-12-27 20:57:11 +0300

    add err handling fn to hero.service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -43,6 +43,18 @@ export class HeroService {
     );
   }
 
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url).pipe(
+      map(heroes => heroes[0]),
+      tap(h => {
+        const outcome = h ? `fetched` : `did not find`;
+        this.log(`${outcome} hero id=${id}`);
+      }),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  } 
+
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
